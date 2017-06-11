b20c4e651f1cd6f127a112e3ea1f2ecadb149321
karthik <buntyboy.1990@gmail.com>
2017-06-12 00:21:38 +0530

    added sample java program

---
+++ HelloWorld.java
@@ -0,0 +1,12 @@
+/**
+ * Created by karthik on 09-04-2017.
+ */
+public class HelloWorld
+{
+    public static void main(String []args){
+        for(int i=1;i<=10;i++)
+        {
+            System.out.println("Hello World");
+        }
+    }
+}
